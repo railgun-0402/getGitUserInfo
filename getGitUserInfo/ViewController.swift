99d3d34dae23caab1b14576d2024e23be1b61972
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2022-08-11 10:30:54 +0900

    Initial Commit

---
+++ getGitUserInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getGitUserInfo
+//
+//  Created by 菅原大輝 on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
